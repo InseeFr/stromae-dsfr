f2c50eb7a13eec20e0d922941289e40bbe3cc527
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-09-04 16:07:47 +0200

    fix: tooltip tracker

--- src/shared/telemetry/trackers/TooltipTracker.ts
+++ src/shared/telemetry/trackers/TooltipTracker.ts
@@ -1,23 +1,23 @@
-import { TooltipTrackingInstrumentation } from '../instrumentations/TooltipTrackingInstrumentation'
 import type { TelemetryCollector } from '../type'
 
 export function addTooltipTracker(collector: TelemetryCollector) {
-  const listener = (e: MouseEvent) => {
+  const listener = (e: Event) => {
     const target = e.target as HTMLElement
-    if (target.hasAttribute(TooltipTrackingInstrumentation.dataAttribute)) {
-      const spanName = `Tooltip Tracking - ${target.id || target.className}`
+
+    if (target.hasAttribute('data-fr-js-tooltip')) {
+      const spanName = `tooltip.show`
       collector.pushEvent({
         name: spanName,
         attributes: {
-          'tooltip.innerText': target.innerText,
+          'tooltip.innerText':
+            target.previousElementSibling?.textContent ?? undefined,
           'tooltip.elementId': target.id,
           'tooltip.elementClass': target.className,
-          'tooltip.state': target.classList.contains('fr-tooltip--shown')
-            ? 'visible'
-            : 'hidden',
         },
       })
     }
   }
-  document.addEventListener('mouseover', listener)
+  document.documentElement.addEventListener('dsfr.show', listener, {
+    capture: true,
+  })
 }

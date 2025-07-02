2aa231eec9e72985811280464f6b094d5053f418
QRuhier <QRuhier@users.noreply.github.com>
2025-07-02 08:04:05 +0000

    deploy: 7f36d88fd2d3f0d4b14efbdd55a345f6e4b923d0

--- storybook/assets/Sequence.stories-OCy-irHJ.js
+++
@@ -1,9 +0,0 @@
-import{O as u}from"./Orchestrator-8MsW4Oqo.js";import{s as m,a as i}from"./question-GgRePy4t.js";import"./index-DLH9COqB.js";import"./index-B-xgG2PE.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-CGHx0_JJ.js";import"./i18n-B9XdcG1J.js";const q={title:"Components/Sequence",component:u},r={args:{source:m}},e={args:{source:i}};var s,o,t;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    source: defaultSource
-  }
-} satisfies Story`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var a,c,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    source: questionSource
-  }
-} satisfies Story`,...(n=(c=e.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const x=["Default","Question"];export{r as Default,e as Question,x as __namedExportsOrder,q as default};

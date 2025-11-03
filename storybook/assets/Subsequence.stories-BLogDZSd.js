7a48a2d1ee4416ef96d14f05090396a32708e254
laurentC35 <laurentC35@users.noreply.github.com>
2025-11-03 08:22:41 +0000

    deploy: dc7d9e0560e81178e14099077267eefd021b4745

---
+++ storybook/assets/Subsequence.stories-BLogDZSd.js
@@ -0,0 +1,9 @@
+import{O as u}from"./Orchestrator-Claq7FQ9.js";import{s as m,a as i}from"./question-GgRePy4t.js";import"./index-BJ_vSxth.js";import"./index-B-xgG2PE.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DtK-5Yz-.js";import"./i18n-WExJy0HY.js";const q={title:"Components/Subsequence",component:u},r={args:{source:m}},e={args:{source:i}};var s,o,t;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    source: defaultSource
+  }
+} satisfies Story`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var a,c,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    source: questionSource
+  }
+} satisfies Story`,...(n=(c=e.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const x=["Default","Question"];export{r as Default,e as Question,x as __namedExportsOrder,q as default};

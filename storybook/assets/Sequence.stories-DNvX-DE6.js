1775f19789c8b9be77b23701a1f9ee46897f31f3
laurentC35 <laurentC35@users.noreply.github.com>
2025-12-30 15:35:05 +0000

    deploy: d0695ad5091ba97cc0f07dc95cf9d40cd9ff7a2c

---
+++ storybook/assets/Sequence.stories-DNvX-DE6.js
@@ -0,0 +1,9 @@
+import{O as s}from"./Orchestrator-DlxdD0Xt.js";import{s as o,a as t}from"./question-GgRePy4t.js";import"./index-k4lGdB2g.js";import"./index-C2khmikk.js";import"./_commonjsHelpers-CE1G-McA.js";import"./index-QJeYjUx7.js";import"./i18n-f_xwKfc8.js";const d={title:"Components/Sequence",component:s},r={args:{source:o}},e={args:{source:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    source: defaultSource
+  }
+} satisfies Story`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    source: questionSource
+  }
+} satisfies Story`,...e.parameters?.docs?.source}}};const f=["Default","Question"];export{r as Default,e as Question,f as __namedExportsOrder,d as default};

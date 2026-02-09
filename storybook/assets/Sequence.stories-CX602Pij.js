dcb61d5b2ef2f4c696fd1214cda3016c7e7b72f5
laurentC35 <laurentC35@users.noreply.github.com>
2026-02-09 13:51:04 +0000

    deploy: 2c1153e43c1201e01a9ccb74906cfef08c702ef5

--- storybook/assets/Sequence.stories-CX602Pij.js
+++
@@ -1,9 +0,0 @@
-import{O as s}from"./Orchestrator-BrAVcbRJ.js";import{s as o,a as t}from"./question-GgRePy4t.js";import"./index--sOxJmJX.js";import"./_commonjsHelpers-CE1G-McA.js";import"./index-CPmPM_fc.js";import"./index-DginCt-p.js";import"./i18n-CrjTLhaZ.js";const d={title:"Components/Sequence",component:s},r={args:{source:o}},e={args:{source:t}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    source: defaultSource
-  }
-} satisfies Story`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    source: questionSource
-  }
-} satisfies Story`,...e.parameters?.docs?.source}}};const f=["Default","Question"];export{r as Default,e as Question,f as __namedExportsOrder,d as default};

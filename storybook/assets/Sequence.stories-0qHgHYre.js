bb9c451d74a37e33e22f2d69bb30bc5473a0f5a7
MailineN <MailineN@users.noreply.github.com>
2026-08-04 12:57:42 +0000

    deploy: 10b4388451680f0226610b7a21789952bb36447d

--- storybook/assets/Sequence.stories-0qHgHYre.js
+++
@@ -1,9 +0,0 @@
-import{O as s}from"./Orchestrator-2eLiRg97.js";import{s as o,a}from"./question-GgRePy4t.js";import"./iframe-BSL8MZuZ.js";import"./preload-helper-PPVm8Dsz.js";const m={title:"Components/Sequence",component:s},r={args:{source:o}},e={args:{source:a}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    source: defaultSource
-  }
-} satisfies Story`,...r.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    source: questionSource
-  }
-} satisfies Story`,...e.parameters?.docs?.source}}};const i=["Default","Question"];export{r as Default,e as Question,i as __namedExportsOrder,m as default};
